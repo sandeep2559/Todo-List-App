9d6a7d78130d628e63ff39cd06fa2b68fa9ef3db
Sandeep Kumar <57530878+sandeep2559@users.noreply.github.com>
2021-06-24 17:52:11 +0530

    port changes

--- app.js
+++ app.js
@@ -150,7 +150,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
